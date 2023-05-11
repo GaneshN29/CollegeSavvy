2fe42b3fcc479cd84bfc9243a072b6d020bd24ff
ganeshsnayak <ganeshsnayak29@gmail.com>
2023-05-11 16:25:09 +0530

    first commit

--- app/src/main/java/com/example/mylife/ADDANS.java
+++ app/src/main/java/com/example/mylife/ADDANS.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Alarm extends AppCompatActivity {
+public class ADDANS extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alarm);
+        setContentView(R.layout.activity_addans);
     }
 }
